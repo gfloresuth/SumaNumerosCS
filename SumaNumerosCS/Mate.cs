bce5ddbc0bcc3bfb6d11b343563ac71dfacef3b9
gfloresuth <dexterminio@gmail.com>
2016-01-08 15:55:03 -0700

    Segunda entrada, se agregaron mas elementos

---
+++ SumaNumerosCS/Mate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumaNumerosCS
+{
+    public class Mate
+    {
+        public int PrimerNumero;
+        public int SegundoNumero;
+        private int Resultado;
+        public void EstablecerPrimerNumero(string unValor)
+        {
+            PrimerNumero = int.Parse(unValor);
+        }
+        public void EstablecerSegundoNumero(string unValor)
+        {
+            SegundoNumero = int.Parse(unValor);
+        }
+        public void CalcularSuma()
+        {
+            Resultado = PrimerNumero + SegundoNumero;
+        }
+        public int ObtenerResultado()
+        {
+            return Resultado;
+        }
+    }
+}
--- SumaNumerosCS/Program.cs
+++ SumaNumerosCS/Program.cs
@@ -8,7 +8,28 @@ namespace SumaNumerosCS
     {
         static void Main(string[] args)
         {
+            Mate m;
+            m = new Mate();
+            try
+            {
+                Console.Write("Primer Numero: ");
+                m.EstablecerPrimerNumero(Console.ReadLine());
+                Console.Write("Segundo Numero: ");
+                m.EstablecerSegundoNumero(Console.ReadLine());
 
+                m.CalcularSuma();
+
+                Console.WriteLine("El resultado es: " + m.ObtenerResultado());
+            }
+            catch (Exception unError)
+            {
+                Console.WriteLine("Error: " + unError.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Termino programa.");
+            }
+            Console.ReadKey();
         }
     }
 }
